a02415a48dc26f951dfe744820984fa00383d73f
louvang <vanglou@proton.me>
2025-05-13 22:18:19 -0500

    Add header and footer to layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Link from 'next/link';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -16,7 +17,53 @@ export default function RootLayout({
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/won7smw.css" />
       </head>
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        <header className="layout-header w-full px-20">
+          <div className="py-7 flex justify-between items-center border-b">
+            <div className="logo font-serif text-4xl">Lou Vang</div>
+            <nav className="top-nav">
+              <ul className="font-mono lowercase list-none flex justify-between w-[400px]">
+                <li className="top-nav-item">
+                  <Link href="/work">Work</Link>
+                </li>
+                <li className="top-nav-item">
+                  <Link href="/blog">Blog</Link>
+                </li>
+                <li className="top-nav-item">
+                  <Link href="/about">About</Link>
+                </li>
+                <li className="top-nav-item">
+                  <Link href="/contact">Contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+        <main>{children}</main>
+        <footer className="layout-footer w-full">
+          <div className="px-20 py-10 flex justify-between items-center border-b text-lg">
+            <div className="copyright-line">
+              &copy; 2025 Lou Vang. All rights reserved.
+            </div>
+            <nav className="bottom-nav">
+              <ul className="list-none flex justify-between w-[280px]">
+                <li className="bottom-nav-item">
+                  <Link href="/work">Home</Link>
+                </li>
+                <li className="bottom-nav-item">
+                  <Link href="/blog">Blog</Link>
+                </li>
+                <li className="bottom-nav-item">
+                  <Link href="/rss">RSS</Link>
+                </li>
+                <li className="bottom-nav-item">
+                  <Link href="/contact">Contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </footer>
+      </body>
     </html>
   );
 }
