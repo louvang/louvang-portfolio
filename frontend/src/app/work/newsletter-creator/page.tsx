4cfe95199e22ce5aa7aabdbe6fc6ac4c37c75b15
louvang <vanglou@proton.me>
2025-06-03 22:51:31 -0500

    Add base individual portfolio pages

---
+++ frontend/src/app/work/newsletter-creator/page.tsx
@@ -0,0 +1,20 @@
+export default function NewsletterCreator() {
+  return (
+    <div className="page-minheight">
+      <div className="page-minheight flex justify-center items-center">
+        <div className="coming-soon">
+          <div className="coming-soon__heading-row">
+            <img
+              src="../images/icons/gearbox.svg"
+              alt=""
+              className="coming-soon__gear-icon"
+            />
+            <div className="coming-soon__heading">Coming Soon</div>
+          </div>
+
+          <p className="coming-soon__p">This page is under construction.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
